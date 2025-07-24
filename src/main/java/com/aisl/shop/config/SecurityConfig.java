9299140164aa69fb56fc9507d6632ce4226f7991
MINDONGDA <alsehd2414@naver.com>
2025-07-24 15:49:48 +0900

    feat: cart

--- src/main/java/com/aisl/shop/config/SecurityConfig.java
+++ src/main/java/com/aisl/shop/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                                 "/users/password",
                                 "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
                                         "/images/**","/upload/**","/favicon.ico","/orders/**", "/wishlist/**", "/reviews/**",
-                                "/habits/**", "/diaries/**"
+                                "/habits/**", "/diaries/**", "/cart/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
